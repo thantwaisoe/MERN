1266c5aba8b96e73368bae0512d747f41329ab3a
thantwaisoe <thantwaisoe456@gmail.com>
2023-01-04 01:16:20 +0630

    create components and add tailwind css

--- login-app/client/src/App.js
+++ login-app/client/src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import PageNotFound from './components/PageNotFound';
+// import all routes
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Username from './components/Username';
 
-function App() {
+
+
+// root routes
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Username/>
+    },
+    {
+        path: '/register',
+        element: <Register/>
+    },
+    {
+        path: '/recovery',
+        element: <Recovery/>
+    },
+    {
+        path: '/password',
+        element: <Password/>
+    },
+    {
+        path: '/profile',
+        element: <Profile/>
+    },
+    {
+        path: '/reset',
+        element: <Reset/>
+    },
+    {
+        path: '*',
+        element: <PageNotFound/>
+    }
+])
+export default function App() {
   return (
-    <div className="App">
-      <h2>React App</h2>
-    </div>
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
-
-export default App;
---
+++ login-app/client/src/components/Recovery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Recovery() {
+  return (
+    <div>
+      <h1>Recovery</h1>
+    </div>
+  )
+}
